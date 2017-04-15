2d88279b44bb718ee6f810f6a9cfb90997b01683
Plamen <plamen.atanasov91@gmail.com>
2017-04-15 23:53:13 +0300

    finished last tasks from hqc2 hw

---
+++ C#/06. High Quality Code Part 2/05. Development Tools/Task 2. Simple T4 Template/T4Template/CreateSomeClasses.cs
@@ -0,0 +1,85 @@
+﻿
+	public class Class0 
+	{
+		private int number;
+
+		public Class0(int number)
+		{
+			this.Number = number;
+		}
+
+		public int Number
+		{
+			get
+			{
+				return this.number;
+			}
+
+			private set
+			{
+				this.number = value;
+			}
+		}
+
+		public bool SomeMethod()
+		{
+			return true;
+		}
+	}
+	
+	public class Class1 
+	{
+		private int number;
+
+		public Class1(int number)
+		{
+			this.Number = number;
+		}
+
+		public int Number
+		{
+			get
+			{
+				return this.number;
+			}
+
+			private set
+			{
+				this.number = value;
+			}
+		}
+
+		public bool SomeMethod()
+		{
+			return true;
+		}
+	}
+	
+	public class Class2 
+	{
+		private int number;
+
+		public Class2(int number)
+		{
+			this.Number = number;
+		}
+
+		public int Number
+		{
+			get
+			{
+				return this.number;
+			}
+
+			private set
+			{
+				this.number = value;
+			}
+		}
+
+		public bool SomeMethod()
+		{
+			return true;
+		}
+	}
+	
---
+++ C#/06. High Quality Code Part 2/05. Development Tools/Task 2. Simple T4 Template/T4Template/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4Template
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Class0 someClass = new Class0(2);
+            Console.WriteLine(someClass.Number);
+            Console.WriteLine(someClass.SomeMethod());
+        }
+    }
+}
