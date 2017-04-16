ef36a7d017ff29897420e84a960db88a893c156d
Plamen <plamen.atanasov91@gmail.com>
2017-04-16 12:03:31 +0300

    renamed folder

--- C#/06. High Quality Code Part 2/05. Development Tools/TestMatrix/StartUp.cs
+++ C#/06. High Quality Code Part 2/05. Development Tools/TestMatrix/StartUp.cs
@@ -24,18 +24,6 @@
 
             Console.WriteLine("[any key to exit]");
             Console.ReadKey();
-
-            // Matrix defaultMatrix = new Matrix();
-            // defaultMatrix.Fill();
-            // Console.WriteLine("Matrix of default size 3*3");
-            // Console.WriteLine(defaultMatrix);
-               
-            // Console.WriteLine();
-               
-            // Matrix matrix = new Matrix(5);
-            // matrix.Fill();
-            // Console.WriteLine("Matrix of size 5*5");
-            // Console.WriteLine(matrix);
         }
     }
 }
